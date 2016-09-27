de93f0e57010a35c7c4b161ea4e17caef088e5bb
Chris Morales <lambda.designs.cm@gmail.com>
2016-09-27 15:41:10 -0700

    added screen shot for statement coverage

--- unittest_coverage_talk/tests/test_example2.py
+++ unittest_coverage_talk/tests/test_example2.py
@@ -10,15 +10,15 @@ class GetNumberTest(unittest.TestCase):
         from unittest_coverage_talk.example2 import get_number
         return get_number(odd_number)
 
-    # def test_get_number_return_int_value_of_one(self):
-    #     """
-    #     Given a word in the list we expect the function to return True.
-    #     """
-    #     test_odd_number = True
-    #     expected_number = 1
-    #     returned_number = self._callFUT(test_odd_number)
+    def test_get_number_return_int_value_of_one(self):
+        """
+        Given a word in the list we expect the function to return True.
+        """
+        test_odd_number = True
+        expected_number = 1
+        returned_number = self._callFUT(test_odd_number)
 
-    #     self.assertEqual(expected_number, returned_number)
+        self.assertEqual(expected_number, returned_number)
 
 
     # def test_get_number_return_int_value_of_two(self):
